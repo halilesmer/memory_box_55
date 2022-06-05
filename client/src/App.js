8f34e0af6f86459c301479ce9705828616699d7d
halilesmer <halil@esmer.de>
2022-06-06 00:36:16 +0200

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomeScreen from './Screens/HomeScreen';
+import CreateScreen from './Screens/CreateScreen';
 
 
 function App() {
   return (
     <div className="App">
-    Merhaba 
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/create" element={<CreateScreen />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Screens/CreateScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateScreen = () => {
+  return (
+    <div>CreateScreen</div>
+  )
+}
+
+export default CreateScreen
